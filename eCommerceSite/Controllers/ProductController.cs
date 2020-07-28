95a5f31ce470b6a3e9db9cfbfb2f3be36d26c55d
Motionlessness <64345027+Motionlessness@users.noreply.github.com>
2020-07-28 11:28:01 -0700

    #5 add products to db (#15)
    * added AddProducts controller
    
    * Made Add products controller and html

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -27,5 +27,28 @@ namespace eCommerceSite.Controllers
             //Send list of products to view to be displayed
             return View(products);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add to db
+                _context.Products.Add(p);
+                _context.SaveChanges();
+
+                TempData["Message"] = $"Item #{p.ProductId} : {p.Title} was added successfully!";
+                //redirect back to catalog page
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
